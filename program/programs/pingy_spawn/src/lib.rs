ace80706abd75a22b7426671676db3236e2c4a34
BigHitterG <thomas.goetz.jr@gmail.com>
2026-03-24 09:05:29 -0500

    Update program ID in lib.rs

--- program/programs/pingy_spawn/src/lib.rs
+++ program/programs/pingy_spawn/src/lib.rs
@@ -4,7 +4,7 @@ use anchor_lang::system_program;
 use anchor_spl::associated_token::AssociatedToken;
 use anchor_spl::token::{self, Mint, MintTo, Token, TokenAccount, Transfer};
 
-declare_id!("FAe6a8TXM2bcrFftfMtSNQ6CsxCXvxKcCUHtxaWYPDHi");
+declare_id!("5RRdaiZNyBCcB9ohpp4KnYsCbfNeEnPRGKocNvepW5Yk");
 
 pub const SPAWN_FEE_BPS: u64 = 100;
 pub const BPS_DENOM: u64 = 10_000;
